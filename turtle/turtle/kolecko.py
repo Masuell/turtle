620ad5e8cdf8000e2fb778c3a69041c8c1d7e357
VS 2017 <vs2017.test@outlook.com>
2018-06-06 13:59:16 +0200

    initial

---
+++ turtle/turtle/kolecko.py
@@ -0,0 +1,20 @@
+import turtle
+from turtle import *
+
+def nakresli_kruh(tvar,velkost,farba,stupanie,polomer):
+    tess=turtle.Turtle()
+    tess.shape(tvar)
+    tess.color(farba)
+    tess.penup()
+
+    for x in range(int(360/stupanie)):
+        tess.forward(polomer)
+        tess.stamp()
+        tess.left(180)
+        tess.forward(polomer)
+        tess.left(180)
+        tess.left(stupanie)
+    tess.penup()
+    tess.forward(polomer)
+
+nakresli_kruh('turtle',5,'red',20,100)
